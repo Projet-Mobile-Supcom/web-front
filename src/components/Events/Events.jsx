86b98e25e3930a3a54de9d2faff8b779ad1c8918
Sami Kammoun <kammounsami77@gmail.com>
2023-01-10 02:38:19 +0100

    Auth & main events page

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,7 +1,70 @@
 import React from "react";
+import "./events.css";
 
-function Events() {
-  return <div>Events</div>;
-}
+const Events = () => {
+  const events = [
+    {
+      id: 1,
+      title: "Event 1",
+      short_description:
+        "Summer concert series featuring local talent and delicious food. Bring blankets or chairs and enjoy the music under the stars. Family-friendly and fun for all ages.",
+      date: "2021-09-01",
+      banner: "https://picsum.photos/2000/300",
+    },
+    {
+      id: 2,
+      title: "Event 2",
+      short_description:
+        "Summer concert series featuring local talent and delicious food. Bring blankets or chairs and enjoy the music under the stars. Family-friendly and fun for all ages. ",
+      date: "2021-09-02",
+      banner: "https://picsum.photos/2000/300",
+    },
+    {
+      id: 3,
+      title: "Event 2",
+      short_description:
+        "Summer concert series featuring local talent and delicious food. Bring blankets or chairs and enjoy the music under the stars. Family-friendly and fun for all ages. ",
+      date: "2021-09-02",
+      banner: "https://picsum.photos/2000/300",
+    },
+    {
+      id: 4,
+      title: "Event 2",
+      short_description:
+        "Summer concert series featuring local talent and delicious food. Bring blankets or chairs and enjoy the music under the stars. Family-friendly and fun for all ages. ",
+      date: "2021-09-02",
+      banner: "https://picsum.photos/2000/300",
+    },
+    {
+      id: 5,
+      title: "Event 2",
+      short_description:
+        "Summer concert series featuring local talent and delicious food. Bring blankets or chairs and enjoy the music under the stars. Family-friendly and fun for all ages. ",
+      date: "2021-09-02",
+      banner: "https://picsum.photos/2000/300",
+    },
+  ];
+  return (
+    <section className="events">
+      <h2>Created Events</h2>
+
+      <div className="events__container">
+        {events.map((event) => (
+          <div key={event.id} className="event__card">
+            <div className="event__banner" style={{ backgroundImage: `url(${event.banner})` }}></div>
+            <div className="event__details">
+              <h1>{event.title}</h1>
+              <div className="event__description">{event.short_description}</div>
+              <div className="two__columns">
+                <div className="event__date">{event.date}</div>
+                <div className="btn">Go To</div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default Events;
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "./login.css";
+import "../Signup/signup.css";
 import { useState } from "react";
 
-const Login = () => {
-  const [submissionResults, setsubmissionResults] = useState("Login to continue");
+const Signup = () => {
+  const [submissionResults, setsubmissionResults] = useState("Sign up to continue");
   return (
     <div className="container row">
       <div className="logo__container">
@@ -11,24 +11,29 @@ const Login = () => {
       </div>
       <div className="login__container">
         <form className="login__form">
-          <h1>Sign In</h1>
+          <h1>Sign Up</h1>
           <div className="credentials">
+            <div className="two__columns">
+              <input type={"text"} name="name" placeholder="John"></input>
+              <input type={"text"} name="lastName" placeholder="Cena"></input>
+            </div>
             <input type={"email"} name="email" placeholder="youremail@mail.co"></input>
             <input type={"password"} name="password" placeholder="Your Password"></input>
           </div>
           <div className="submission">
             <button className="btn btn-primary" type="submit">
-              Sign In
+              Sign Up
             </button>
+
             <h5>{submissionResults}</h5>
           </div>
         </form>
       </div>
       <div className="other__options">
-        <a href="/signup">Don't have an account ?</a>
+        <a href="/">Already have an account ?</a>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default Signup;
