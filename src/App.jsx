5ec638d6aea4f1a1b73424fa63cedd8ed54aba76
Sami Kammoun <kammounsami77@gmail.com>
2023-01-10 02:36:30 +0100

    Added routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Login from "./components/Login/Login";
+import Events from "./components/Events/Events";
+import Signup from "./components/Signup/Signup";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 const App = () => {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Login />}></Route>
+        <Route exact path="/signup" element={<Signup />}></Route>
+        <Route exact path="/events" element={<Events />}></Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
