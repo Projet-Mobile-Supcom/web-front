8551e7d0f8e35ffb2f9bfa4eead571d155b89638
Sami Kammoun <kammounsami77@gmail.com>
2023-01-10 19:30:20 +0100

    Upload image

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./components/Login/Login";
 import Events from "./components/Events/Events";
 import Signup from "./components/Signup/Signup";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Upload from "./components/Upload/Upload";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route exact path="/" element={<Login />}></Route>
         <Route exact path="/signup" element={<Signup />}></Route>
         <Route exact path="/events" element={<Events />}></Route>
+        <Route exact path="/upload" element={<Upload />}></Route>
       </Routes>
     </Router>
   );
